45ac1195a4bf9015f08f73c1a1a3d92ed03f1331
Ronald SG <ronald132@gmail.com>
2021-06-07 17:05:55 +0800

    add test.ejs

--- routes/main.js
+++ routes/main.js
@@ -8,4 +8,15 @@ router.get("/postback", function (req, res) {
   res.render("postback");
 });
 
+router.get("/rich", function (req, res) {
+  res.render("rich");
+});
+
+router.get("/jstag", function (req, res) {
+  res.render("jstag");
+});
+router.get("/test", function (req, res) {
+  res.render("test");
+});
+
 module.exports = router;
